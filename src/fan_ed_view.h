7591afe978c1aa9d5a484d4f369d69e7bd136323
xwj01 <40065857+xwj01@users.noreply.github.com>
2021-09-18 21:41:18 +0800

    Delete src directory

--- src/fan_ed_view.h
+++
@@ -1,9 +0,0 @@
-#ifndef FAN_ED_VIEW_H
-#define FAN_ED_VIEW_H
-
-#include <torch/extension.h>
-
-torch::Tensor prj_fan_ed_view_cuda(torch::Tensor image, torch::Tensor options, torch::Tensor prj_views);
-torch::Tensor bprj_fan_ed_view_cuda(torch::Tensor projection, torch::Tensor options, torch::Tensor prj_views);
-
-#endif

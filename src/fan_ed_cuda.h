ba8bdba56071f24317a9d0975421b822ac3228e0
xwj01 <40065857+xwj01@users.noreply.github.com>
2023-02-25 15:08:55 -0500

    Update fan_ed_cuda.h

--- src/fan_ed_cuda.h
+++ src/fan_ed_cuda.h
@@ -7,6 +7,7 @@ torch::Tensor prj_fan_ed_cuda(torch::Tensor image, torch::Tensor options);
 torch::Tensor prj_t_fan_ed_cuda(torch::Tensor projection, torch::Tensor options);
 torch::Tensor bprj_t_fan_ed_cuda(torch::Tensor image, torch::Tensor options);
 torch::Tensor bprj_fan_ed_cuda(torch::Tensor image, torch::Tensor options);
+torch::Tensor bprj_sv_fan_ed_cuda(torch::Tensor image, torch::Tensor options);
 torch::Tensor fbp_fan_ed_cuda(torch::Tensor projection, torch::Tensor options);
 
 #endif

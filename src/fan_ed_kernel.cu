80d3e2f297e3cb6d0b85c6713579c24137514e84
xwj01 <40065857+xwj01@users.noreply.github.com>
2023-02-25 15:08:29 -0500

    Update fan_ed_kernel.cu

--- src/fan_ed_kernel.cu
+++ src/fan_ed_kernel.cu
@@ -942,6 +942,224 @@ __global__ void rlfilter(scalar_t* __restrict__ filter,
     }
 }
 
+template <typename scalar_t>
+__global__ void bprj_sv_fan_ed(
+    torch::PackedTensorAccessor<scalar_t,4,torch::RestrictPtrTraits,size_t> image,
+    const torch::PackedTensorAccessor<scalar_t,4,torch::RestrictPtrTraits,size_t> projection,
+    const scalar_t* __restrict__ views, const scalar_t* __restrict__ dets,
+    const scalar_t* __restrict__ width, const scalar_t* __restrict__ height,
+    const scalar_t* __restrict__ dImg, const scalar_t* __restrict__ dDet,
+    const scalar_t* __restrict__ Ang0, const scalar_t* __restrict__ dAng, 
+    const scalar_t* __restrict__ s2r, const scalar_t* __restrict__ d2r, 
+    const scalar_t* __restrict__ binshift) {
+    
+    __shared__ unsigned int nblocks;
+    __shared__ unsigned int idxchannel;
+    __shared__ unsigned int idxview;
+    nblocks = ceil(*views / gridDim.y);
+    idxchannel = blockIdx.x % nblocks;
+    idxview = idxchannel * gridDim.y + blockIdx.y;
+    if (idxview >= *views)   return;
+    idxchannel = blockIdx.x / nblocks;
+    __shared__ scalar_t prj[BLOCK_DIM];
+    __shared__ scalar_t dPoint[BLOCK_DIM];
+    __shared__ scalar_t dImage;    
+    __shared__ scalar_t sourcex;
+    __shared__ scalar_t sourcey;   
+    __shared__ scalar_t d0x;
+    __shared__ scalar_t d0y;
+    __shared__ scalar_t dPoint0;
+    __shared__ scalar_t s0;
+    __shared__ double ang;
+    __shared__ double PI;
+    __shared__ double ang_error;
+    __shared__ double cosval;
+    __shared__ double sinval;
+    __shared__ double virdDet;
+    __shared__ double virshift;
+    __shared__ unsigned int dIndex0;
+
+    PI = acos(-1.0);
+    ang = idxview * *dAng + *Ang0;
+    ang_error = abs(ang - round(ang / PI) * PI) * 4 / PI;
+    cosval = cos(ang);
+    sinval = sin(ang);
+    sourcex = - sinval * *s2r;
+    sourcey = cosval * *s2r;
+    virdDet = *s2r / (*s2r + *d2r) * *dDet;
+    virshift = *s2r / (*s2r + *d2r) * *binshift;
+    s0 = *s2r;
+    dImage = *dImg;
+    dIndex0 = blockIdx.z * blockDim.x;
+    unsigned int tx = threadIdx.x;
+    unsigned int dIndex = dIndex0 + tx;
+    __syncthreads();
+    if (ang_error <= 1) {
+        ang_error = (ang - floor(ang / 2 / PI) * 2 * PI) * 4 / PI;
+        if (ang_error >= 3 && ang_error < 7) {
+            d0x = ((*dets / 2 - dIndex0) * virdDet +  virshift) * cosval;
+            d0y = ((*dets / 2 - dIndex0) * virdDet +  virshift) * sinval;
+            dPoint0 = map_x(sourcex, sourcey, d0x, d0y);
+            if (dIndex < *dets) {
+                scalar_t detx = ((*dets / 2 - dIndex - 1) * virdDet +  virshift) * cosval;
+                scalar_t dety = ((*dets / 2 - dIndex - 1) * virdDet +  virshift) * sinval;
+                dPoint[tx] = map_x(sourcex, sourcey, detx, dety);
+            }
+        } else {
+            d0x = ((dIndex0 - *dets / 2) * virdDet +  virshift) * cosval;
+            d0y = ((dIndex0 - *dets / 2) * virdDet +  virshift) * sinval;
+            dPoint0 = map_x(sourcex, sourcey, d0x, d0y);
+            if (dIndex < *dets) {
+                scalar_t detx = ((dIndex + 1 - *dets / 2) * virdDet +  virshift) * cosval;
+                scalar_t dety = ((dIndex + 1 - *dets / 2) * virdDet +  virshift) * sinval;
+                dPoint[tx] = map_x(sourcex, sourcey, detx, dety);
+            }
+        }
+        __syncthreads();        
+        if (dIndex < *dets) {
+            if (ang_error >= 3 && ang_error < 7) {
+                prj[tx] = projection[idxchannel][0][idxview][static_cast<unsigned int>(*dets)-1-dIndex];
+            } else {
+                prj[tx] = projection[idxchannel][0][idxview][dIndex];
+            }
+        } else {
+            prj[tx] = 0;
+        }
+        __syncthreads();
+        for (int i = 0; i < ceil(*height / blockDim.x); i++){
+            int idxrow = i * blockDim.x + tx;
+            if (idxrow < *height) {
+                scalar_t i0y = (*height / 2 - idxrow - 0.5) * dImage;
+                scalar_t i0x = - *width / 2 * dImage; 
+                int idx0col = floor(((i0y - sourcey) / (d0y - sourcey) * 
+                    (d0x - sourcex) + sourcex - i0x) / dImage);
+                idx0col = max(idx0col, 0);
+                i0x += idx0col * dImage;
+                scalar_t threadprj = 0;
+                scalar_t prebound = map_x(sourcex, sourcey, i0x, i0y);
+                scalar_t prepixbound = prebound;
+                prebound = max(prebound, dPoint0);
+                i0x += dImage;
+                scalar_t pixbound = map_x(sourcex, sourcey, i0x, i0y);
+                scalar_t detbound = dPoint[0];
+                int idxd = 0, idxi = idx0col;
+                while (idxi < *width && (idxd + dIndex0) < *dets && idxd < blockDim.x) {
+                    if (detbound <= prebound) {
+                        idxd ++;                        
+                        if (idxd < blockDim.x) detbound = dPoint[idxd];
+                    }else if (pixbound <= prebound){
+                        idxi ++;
+                        i0x += dImage;
+                        prepixbound = pixbound;
+                        pixbound = map_x(sourcex, sourcey, i0x, i0y);
+                    }else if (pixbound <= detbound) {
+                        threadprj += (pixbound - prebound) * prj[idxd] / (pixbound - prepixbound);
+                        prebound = pixbound;
+                        threadprj *= cweight(sourcex, sourcey, i0x - dImage / 2, i0y, s0);
+                        atomicAdd(&(image[idxchannel][idxview][idxrow][idxi]), threadprj);
+                        threadprj = 0;
+                        idxi ++;
+                        i0x += dImage;
+                        prepixbound = pixbound;
+                        pixbound = map_x(sourcex, sourcey, i0x, i0y);
+                    } else {
+                        threadprj += (detbound - prebound) * prj[idxd] / (pixbound - prepixbound);
+                        prebound = detbound;
+                        idxd ++;
+                        if (idxd < blockDim.x) detbound = dPoint[idxd];
+                    }
+                }
+                if (threadprj !=0 ) {
+                    threadprj *= cweight(sourcex, sourcey, i0x - dImage / 2, i0y, s0);
+                    atomicAdd(&(image[idxchannel][idxview][idxrow][idxi]), threadprj);
+                } 
+            }
+        }
+    } else {
+        ang_error = (ang - floor(ang / 2 / PI) * 2 * PI) * 4 / PI;
+        if (ang_error >= 3 && ang_error < 7) {
+            d0x = ((*dets / 2 - dIndex0) * virdDet +  virshift) * cosval;
+            d0y = ((*dets / 2 - dIndex0) * virdDet +  virshift) * sinval;
+            dPoint0 = map_y(sourcex, sourcey, d0x, d0y);
+            if (dIndex < *dets) {
+                scalar_t detx = ((*dets / 2 - dIndex - 1) * virdDet +  virshift) * cosval;
+                scalar_t dety = ((*dets / 2 - dIndex - 1) * virdDet +  virshift) * sinval;
+                dPoint[tx] = map_y(sourcex, sourcey, detx, dety);
+            }
+        } else {
+            d0x = ((dIndex0 - *dets / 2) * virdDet +  virshift) * cosval;
+            d0y = ((dIndex0 - *dets / 2) * virdDet +  virshift) * sinval;
+            dPoint0 = map_y(sourcex, sourcey, d0x, d0y);
+            if (dIndex < *dets) {
+                scalar_t detx = ((dIndex + 1 - *dets / 2) * virdDet +  virshift) * cosval;
+                scalar_t dety = ((dIndex + 1 - *dets / 2) * virdDet +  virshift) * sinval;
+                dPoint[tx] = map_y(sourcex, sourcey, detx, dety);
+            }
+        }
+        __syncthreads();
+        if (dIndex < *dets) {
+            if (ang_error >= 3 && ang_error < 7) {
+                prj[tx] = projection[idxchannel][0][idxview][static_cast<unsigned int>(*dets)-1-dIndex];
+            } else {
+                prj[tx] = projection[idxchannel][0][idxview][dIndex];
+            }
+        } else {
+            prj[tx] = 0;
+        }        
+        __syncthreads();
+        
+        for (int i = 0; i < ceil(*width / blockDim.x); i++){
+            int idxcol = i * blockDim.x + tx;
+            if (idxcol < *width) {
+                scalar_t i0x = (idxcol - *width / 2 + 0.5) * dImage;
+                scalar_t i0y = - *height / 2 * dImage; 
+                int idx0row = floor(((i0x - sourcex) / (d0x - sourcex) * 
+                    (d0y - sourcey) + sourcey - i0y) / dImage);
+                idx0row = max(idx0row, 0);
+                i0y += idx0row * dImage;
+                scalar_t threadprj = 0;
+                scalar_t prebound = map_y(sourcex, sourcey, i0x, i0y);
+                scalar_t prepixbound = prebound;
+                prebound = max(prebound, dPoint0);
+                i0y += dImage;
+                scalar_t pixbound = map_y(sourcex, sourcey, i0x, i0y);
+                scalar_t detbound = dPoint[0];
+                int idxd = 0, idxi = idx0row;
+                while (idxi < *height && (idxd + dIndex0) < *dets && idxd < blockDim.x) {
+                    if (detbound <= prebound) {
+                        idxd ++;
+                        if (idxd < blockDim.x) detbound = dPoint[idxd];
+                    }else if (pixbound <= prebound) {
+                        idxi ++;
+                        i0y += dImage;
+                        prepixbound = pixbound;
+                        pixbound = map_y(sourcex, sourcey, i0x, i0y);
+                    }else if (pixbound <= detbound) {
+                        threadprj += (pixbound - prebound) * prj[idxd] / (pixbound - prepixbound);
+                        prebound = pixbound;
+                        threadprj *= cweight(sourcex, sourcey, i0x, i0y - dImage / 2, s0);
+                        atomicAdd(&(image[idxchannel][idxview][static_cast<int>(*height)-1-idxi][idxcol]), threadprj);
+                        threadprj = 0;
+                        idxi ++;
+                        i0y += dImage;
+                        prepixbound = pixbound;
+                        pixbound = map_y(sourcex, sourcey, i0x, i0y);
+                    } else {
+                        threadprj += (detbound - prebound) * prj[idxd] / (pixbound - prepixbound);
+                        prebound = detbound;                        
+                        idxd ++;
+                        if (idxd < blockDim.x) detbound = dPoint[idxd];
+                    }
+                }
+                if (threadprj !=0 ) {
+                    threadprj *= cweight(sourcex, sourcey, i0x, i0y - dImage / 2, s0);
+                    atomicAdd(&(image[idxchannel][idxview][static_cast<int>(*height)-1-idxi][idxcol]), threadprj);
+                } 
+            }
+        }
+    }   
+}
+
 torch::Tensor prj_fan_ed_cuda(torch::Tensor image, torch::Tensor options) {
     cudaSetDevice(image.device().index());
     auto views = options[0];
@@ -1067,6 +1285,40 @@ torch::Tensor bprj_fan_ed_cuda(torch::Tensor projection, torch::Tensor options) 
 
     AT_DISPATCH_FLOATING_TYPES(projection.type(), "fan_beam_equal_distance_fbp_backprojection", ([&] {
         bprj_fan_ed<scalar_t><<<blocks, BLOCK_DIM>>>(
+            image.packed_accessor<scalar_t,4,torch::RestrictPtrTraits,size_t>(),
+            projection.packed_accessor<scalar_t,4,torch::RestrictPtrTraits,size_t>(),
+            views.data<scalar_t>(), dets.data<scalar_t>(), width.data<scalar_t>(),
+            height.data<scalar_t>(), dImg.data<scalar_t>(), dDet.data<scalar_t>(),
+            Ang0.data<scalar_t>(), dAng.data<scalar_t>(), s2r.data<scalar_t>(), 
+            d2r.data<scalar_t>(), binshift.data<scalar_t>()
+        );
+    }));
+    return image;
+}
+
+torch::Tensor bprj_sv_fan_ed_cuda(torch::Tensor projection, torch::Tensor options) {
+    cudaSetDevice(projection.device().index());
+    auto views = options[0];
+    auto dets = options[1];
+    auto width = options[2];
+    auto height = options[3];
+    auto dImg = options[4];
+    auto dDet = options[5];
+    auto Ang0 = options[6];
+    auto dAng = options[7];
+    auto s2r = options[8];
+    auto d2r = options[9];
+    auto binshift = options[10];
+    const int channels = static_cast<int>(projection.size(0));
+    auto image = torch::zeros({channels, views.item<int>(), height.item<int>(), width.item<int>()}, projection.options());
+    
+    int nblocksx = ceil(views.item<float>() / GRID_DIM) * channels;
+    int nblocksy = min(views.item<int>(), GRID_DIM);
+    int nblocksz = ceil(dets.item<float>() / BLOCK_DIM);
+    const dim3 blocks(nblocksx, nblocksy, nblocksz);
+
+    AT_DISPATCH_FLOATING_TYPES(projection.type(), "fan_beam_equal_distance_backprojection_single_view", ([&] {
+        bprj_sv_fan_ed<scalar_t><<<blocks, BLOCK_DIM>>>(
             image.packed_accessor<scalar_t,4,torch::RestrictPtrTraits,size_t>(),
             projection.packed_accessor<scalar_t,4,torch::RestrictPtrTraits,size_t>(),
             views.data<scalar_t>(), dets.data<scalar_t>(), width.data<scalar_t>(),
